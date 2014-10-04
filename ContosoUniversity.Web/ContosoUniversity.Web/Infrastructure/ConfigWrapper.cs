74ce832b494362fd75bc7f7761dba062a5995aab
Jared Wooten <pancakeparfait@gmail.com>
2014-10-04 04:59:34 -0400

    Initial commit
    Initial commit.

---
+++ ContosoUniversity.Web/ContosoUniversity.Web/Infrastructure/ConfigWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Web.Infrastructure
+{
+    public static class ConfigWrapper
+    {
+        public static string ApplicationTitle
+        {
+            get { return ConfigurationManager.AppSettings["ApplicationTitle"]; }
+        }
+    }
+}
